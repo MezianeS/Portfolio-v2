560b7fc2721bd75c1cc3b1e56e3748f9d8afb7b0
SosoMeziane <89013262+MezianeS@users.noreply.github.com>
2022-04-21 14:39:51 +0200

    Header modification

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -6,7 +6,7 @@ function CTA() {
   return (
     <div className="cta">
         <a href="" className='btn'>Download CV</a>
-        <a href="" className='btn'>Let's talk</a>
+        <a href="" className='btn btn-primary'>Let's talk</a>
     </div>
   )
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
+import HeaderSocials from './HeaderSocials'
 
 
 function Header() {
@@ -10,6 +11,17 @@ function Header() {
       <h1>Sofiane Meziane</h1>
       <h4 className='text-light'>Fullstack Developer</h4>
       <CTA />
+
+      <HeaderSocials />
+      
+
+      <div className="me">
+        Une photo
+      </div>
+
+     
+      <a href="#contact" className='scroll__down'>Scroll Down</a>
+
     </div>
   )
 }
