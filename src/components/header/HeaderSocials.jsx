f7e2ebbd0dd3c6fc4ebe4da9409c33310f00f50a
SosoMeziane <89013262+MezianeS@users.noreply.github.com>
2022-04-22 09:58:51 +0200

    fix header
    fix header mobile & medium

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -5,10 +5,17 @@ import {SiCodingame} from 'react-icons/si'
 
 const HeaderSocials = () => {
   return (
-    <div className="header__socials">
-        <a href="https://linkedin.com/" target="_blank"><BsLinkedin/></a>
-        <a href="https://github.com" target="_blank"><BsGithub /></a>
-        <a href="https://codingame.com" target="_blank"><SiCodingame /></a>
+    <div className="header__wrap">
+      <div className="header__socials">
+          <a href="https://linkedin.com/" target="_blank"><BsLinkedin/></a>
+          <a href="https://github.com" target="_blank"><BsGithub /></a>
+          <a href="https://codingame.com" target="_blank"><SiCodingame /></a>
+      </div>
+      <div className="header__socials__mobile">
+          <a href="https://linkedin.com/" target="_blank"><BsLinkedin/></a>
+          <a href="https://github.com" target="_blank"><BsGithub /></a>
+          <a href="https://codingame.com" target="_blank"><SiCodingame /></a>
+      </div>
     </div>
   )
 }
