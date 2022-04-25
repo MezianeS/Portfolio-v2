9ce9c030501267541dbe700f1e31eb1a6eb7d24d
SosoMeziane <89013262+MezianeS@users.noreply.github.com>
2022-04-25 18:00:02 +0200

    Add Contact jsx

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -5,7 +5,20 @@ import './contact.css'
 const Contact = () => {
   return (
     <section id="contact">
-      Contact
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <acticle className="contact__option">
+            <h4>Email</h4>
+            <h5>prosmezds@gmail.com</h5>
+            <a href="mailto:prosmezds@gmail.com"></a>
+          </acticle>
+        </div>
+        {/* END OF CONTACT OPTION */}
+        <form action=""></form>
+      </div>
     </section>
 
   )
