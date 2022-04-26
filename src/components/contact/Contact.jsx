271bc45a113815103e1efd3f35868dd362281782
SosoMeziane <89013262+MezianeS@users.noreply.github.com>
2022-04-26 11:09:14 +0200

    Form css

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -12,24 +12,24 @@ const Contact = () => {
 
       <div className="container contact__container">
         <div className="contact__options">
-          <acticle className="contact__option">
+          <article className="contact__option">
             <MdOutlineEmail className='contact__option-icon' />
             <h4>Email</h4>
             <h5>prosmezds@gmail.com</h5>
             <a href="mailto:prosmezds@gmail.com" target='_blank'>Send a message</a>
-          </acticle>
-          <acticle className="contact__option">
+          </article>
+          <article className="contact__option">
             <RiMessengerLine className='contact__option-icon' />
             <h4>Messenger</h4>
             <h5>Soso</h5>
             <a href="https://m.me/soso" target='_blank'>Send a message</a>
-          </acticle>
-          <acticle className="contact__option">
+          </article>
+          <article className="contact__option">
             <BsWhatsapp className='contact__option-icon' />
             <h4>WhatsApp</h4>
             <h5>+123456789</h5>
             <a href="https://api.whatsapp.com/send?phone=+123456789" target='_blank'>Send a message</a>
-          </acticle>
+          </article>
         </div>
         {/* END OF CONTACT OPTION */}
         <form action="">
