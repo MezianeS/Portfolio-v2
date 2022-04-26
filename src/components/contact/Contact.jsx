63b6e3c9d19c956ca19b8948e49085eb70b1d919
SosoMeziane <89013262+MezianeS@users.noreply.github.com>
2022-04-26 10:00:39 +0200

    Update Contact.jsx
    Add contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './contact.css'
-
+import {MdOutlineEmail} from 'react-icons/md'
+import {RiMessengerLine} from 'react-icons/ri'
+import {BsWhatsapp} from 'react-icons/bs'
 
 const Contact = () => {
   return (
@@ -11,13 +13,26 @@ const Contact = () => {
       <div className="container contact__container">
         <div className="contact__options">
           <acticle className="contact__option">
+            <MdOutlineEmail />
             <h4>Email</h4>
             <h5>prosmezds@gmail.com</h5>
-            <a href="mailto:prosmezds@gmail.com"></a>
+            <a href="mailto:prosmezds@gmail.com">Send a message</a>
+          </acticle>
+          <acticle className="contact__option">
+            <RiMessengerLine />
+            <h4>Messenger</h4>
+            <h5>Soso</h5>
+            <a href="https://m.me/soso">Send a message</a>
+          </acticle>
+          <acticle className="contact__option">
+            <BsWhatsapp />
+            <h4>WhatsApp</h4>
+            <h5>+123456789</h5>
+            <a href="mailto:prosmezds@gmail.com">Send a message</a>
           </acticle>
         </div>
         {/* END OF CONTACT OPTION */}
-        <form action=""></form>
+        <form action=""></form> 
       </div>
     </section>
 
