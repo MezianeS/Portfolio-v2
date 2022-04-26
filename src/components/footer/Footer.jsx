b24f4c2355b0727885cb3253aee6f6b7ce2d286e
SosoMeziane <89013262+MezianeS@users.noreply.github.com>
2022-04-26 15:04:07 +0200

    Update Footer.jsx
    Add buttons & copyright

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
 import './footer.css'
-
+import {BsGithub} from 'react-icons/bs'
+import {BsLinkedin} from 'react-icons/bs'
+import {SiCodingame} from 'react-icons/si'
 
 const Footer = () => {
   return (
     <footer>
+      <a href="#" className='footer__logo'>My Portfolio</a>
+      <ul className="permalinks">
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
 
+      <div className="footer__socials">
+        <a href="https://github.com/"><BsGithub /></a>
+        <a href="https://linkedin.com/"><BsLinkedin /></a>
+        <a href="https://codingame.com/"><SiCodingame /></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Soso MyPortfolio. All rights reserved.</small>
+      </div>
     </footer>
   )
 }
