38cd4c31b8c1a9b4d1788925985b81ab8ba12dea
SosoMeziane <89013262+MezianeS@users.noreply.github.com>
2022-04-26 10:50:50 +0200

    add css contact
    only 3 buttons left

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -13,26 +13,31 @@ const Contact = () => {
       <div className="container contact__container">
         <div className="contact__options">
           <acticle className="contact__option">
-            <MdOutlineEmail />
+            <MdOutlineEmail className='contact__option-icon' />
             <h4>Email</h4>
             <h5>prosmezds@gmail.com</h5>
-            <a href="mailto:prosmezds@gmail.com">Send a message</a>
+            <a href="mailto:prosmezds@gmail.com" target='_blank'>Send a message</a>
           </acticle>
           <acticle className="contact__option">
-            <RiMessengerLine />
+            <RiMessengerLine className='contact__option-icon' />
             <h4>Messenger</h4>
             <h5>Soso</h5>
-            <a href="https://m.me/soso">Send a message</a>
+            <a href="https://m.me/soso" target='_blank'>Send a message</a>
           </acticle>
           <acticle className="contact__option">
-            <BsWhatsapp />
+            <BsWhatsapp className='contact__option-icon' />
             <h4>WhatsApp</h4>
             <h5>+123456789</h5>
-            <a href="mailto:prosmezds@gmail.com">Send a message</a>
+            <a href="https://api.whatsapp.com/send?phone=+123456789" target='_blank'>Send a message</a>
           </acticle>
         </div>
         {/* END OF CONTACT OPTION */}
-        <form action=""></form> 
+        <form action="">
+          <input type="text" name="name" placeholder='Your Full Name' required/>
+          <input type="email" name="email" placeholder='Your Email' required />
+          <textarea name="message" rows="7" placeholder='Your Message' required ></textarea>
+          <button type="submit" className='btn btn-primary'>Send Message</button>
+        </form> 
       </div>
     </section>
 
